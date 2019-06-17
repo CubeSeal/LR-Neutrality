c981fb115e702a36171afb4da93b9c53b82b21da
Kenneth de Silva <kenndesilva1@gmail.com>
2019-06-17 16:39:52 +1000

    Added functions file and streamlined code.

---
+++ functions.R
@@ -0,0 +1,12 @@
+#   Splits vector into list of vectors, each with n items then prods growth
+#   rates to get agg. growth rates.
+
+agg_prod <- function(vector, n) {
+    
+    nr <- length(vector)
+    
+    list_of_vecs <- split(vector, (1:nr) %/% n)
+    return_value <- sapply(list_of_vecs, prod)
+    
+    return(return_value)
+}
--- src.R
+++ src.R
@@ -3,6 +3,9 @@ library(readxl)
 library(dplyr)
 library(ggplot2)
 
+# Functions
+source("./functions.R")
+
 # Import data
 rba_money_aggregates.raw <-
     read_excel("res/rba_money_aggregates.xls",
@@ -18,30 +21,27 @@ abs_5206_key_aggregates.raw <-
 money.df <- inner_join(abs_5206_key_aggregates.raw,
                        rba_money_aggregates.raw,
                        by = 'Date') %>%
-    select(Date, M1 = DMAM1N, NGDP_Q = A2304350J) %>%
-    mutate(M1_Growth = c(M1[-1]/M1[-length(M1)], NA),
+    select(Date, M = DMACN, NGDP_Q = A2304350J) %>%
+    
+    # Percentage growths + 1
+    mutate(M_Growth = c(M[-1]/M[-length(M)], NA),
            NGDP_Q_Growth = c(NGDP_Q[-1]/NGDP_Q[-length(NGDP_Q)], NA)) %>%
     na.omit()
 
-t <- 12
+# Create aggregate df across t quarters
+t <- 4*3
 
 money_g_agg.df <-
-    data.frame(M1_Growth = sapply(split(
-        money.df$M1_Growth,
-        1:nrow(money.df) %/% t
-    ), prod),
-    NGDP_Growth = sapply(split(
-        money.df$NGDP_Q_Growth,
-        1:nrow(money.df) %/% t), prod
-    ))
-    
+    data.frame(M_Growth = agg_prod(money.df$M_Growth, t), 
+               NGDP_Growth = agg_prod(money.df$NGDP_Q_Growth, t))
+
+# LM Model
+money.lm <- lm(NGDP_Growth ~ M_Growth, money_g_agg.df)
+summary(money.lm)
+
 # Graph relationship
-ggplot(money_g_agg.df, aes(x = M1_Growth, y = NGDP_Growth)) + 
+ggplot(money_g_agg.df, aes(x = M_Growth, y = NGDP_Growth)) + 
     geom_point() +
     geom_smooth(method = "lm", formula = y ~ x) +
     geom_abline()
-
-# LM Model
-money.lm <- lm(NGDP_Growth ~ M1_Growth, money_g_agg.df)
-summary(money.lm)
     
